f0d3933b55a52f99b9101d90b4ade424180c2bf2
Errabati, Swathi <212757252@ge.com>
2020-02-22 19:34:59 +0530

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,36 +1,17 @@
-console.log('Client side javascript file is loaded!')
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data);
-//     })
-// })
-
-fetch('/weather?address=' + location).then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error);
-
-        }
-        else {
-            console.log(data.location);
-            console.log(data.forecast);
-        }
-    })
-})
-
-const weatherFrom = document.querySelector('form');
-const search = document.querySelector('input');
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-weatherFrom.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const location = search.value;
-    messageOne.textContent = 'Loading...';
-    messageTwo.textContent = '';
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
